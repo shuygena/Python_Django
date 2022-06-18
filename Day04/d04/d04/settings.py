41e77f89f56e020e928158c6a04d307c38ce7d6c
Malia Skeleton <i.am.zuzya@gmail.com>
2022-06-18 17:23:08 +0300

    add gitignore

--- Day04/d04/d04/settings.py
+++ Day04/d04/d04/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7a%ay_!aaf%t6r5e#$7&$6iw)(ogc*7uh8p%k-w_wj-&4m%+6f'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- Day06/d06/settings.py
+++ Day06/d06/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fdes62a)h-0nya9$fg8byyo8e22p^si6h%-wl5yqlf0s$k-l%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
